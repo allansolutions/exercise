77a54a7fc06e645c8f38234c83cbf42929a620c8
Chris Allan <christopher.john.allan@gmail.com>
2020-07-22 14:55:10 -0500

    Add further instructions

--- pages/report/_id/index.vue
+++ pages/report/_id/index.vue
@@ -12,6 +12,9 @@
   - Transform the response to an array of objects e.g. [ { label: 2016, value: 0 }, { label: 2017, value: 4  }, etc]
   - Add the component 'Chart' to the page and pass int the array of objects as a param
   - Update header to include the report id e.g. "Report 1"
+  - Update the page layout so that on
+    - desktop: layout is 2 columns with Header in left column and Chart in right column
+    - mobile: layout is 1 column with Header appearing above Chart
 */
 
 export default {
