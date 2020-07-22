822829fdbc5fb8e189bb30e6987d29abc54ff403
Chris Allan <christopher.john.allan@gmail.com>
2020-07-22 14:45:03 -0500

    Setup exercise instructions

---
+++ components/Chart.vue
@@ -0,0 +1,31 @@
+<script>
+import { Line } from 'vue-chartjs'
+
+/*
+Instructions:
+Update Chart component to receive an array of objects as a prop e.g [ { label: 2016, value: 0 }, { label: 2017, value: 4 }]
+Update chartDataConfig.labels to be an array of strings, using the lablels in the prop
+Update chartDataConfig.datasets to contain one object whose property 'data' is an array of numbers, using the values in the prop
+*/
+
+export default {
+  extends: Line,
+  props: ['chartData'],
+  data() {
+    return {
+      chartDataConfig: {
+        labels: [],
+        datasets: [
+          {
+            data: [],
+          },
+        ],
+      },
+      options: {},
+    }
+  },
+  mounted() {
+    this.renderChart(this.chartDataConfig, this.options)
+  },
+}
+</script>
---
+++ pages/report/_id/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Report</h1>
+  </div>
+</template>
+
+<script>
+/*
+  Instructions:
+  - Using axios get data from /api/id.json substiting 'id' with the the final param in the route
+  - For testing purposes use http://localhost:3000/report/1
+  - Transform the response to an array of objects e.g. [ { label: 2016, value: 0 }, { label: 2017, value: 4  }, etc]
+  - Add the component 'Chart' to the page and pass int the array of objects as a param
+  - Update header to include the report id e.g. "Report 1"
+*/
+
+export default {
+  name: 'Report',
+  async asyncData({ $axios, params }) {
+    return {}
+  },
+}
+</script>
