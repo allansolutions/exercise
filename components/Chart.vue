82a1f486e584975213d3b414ee146e7d14a3acb1
Chris Allan <christopher.john.allan@gmail.com>
2021-04-19 09:20:26 -0500

    Change from data to computed

--- components/Chart.vue
+++ components/Chart.vue
@@ -1,27 +1,22 @@
 <script>
 import { Line } from 'vue-chartjs'
 
-/*
-Instructions:
-Update Chart component to receive an array of objects as a prop e.g [ { label: 2016, value: 0 }, { label: 2017, value: 4 }]
-Update chartDataConfig.labels to be an array of strings, using the lablels in the prop
-Update chartDataConfig.datasets to contain one object whose property 'data' is an array of numbers, using the values in the prop
-*/
-
 export default {
   extends: Line,
-  data() {
-    return {
-      chartDataConfig: {
+  computed: {
+    chartDataConfig() {
+      return {
         labels: [],
         datasets: [
           {
             data: [],
           },
         ],
-      },
-      options: {},
-    }
+      }
+    },
+    options() {
+      return {}
+    },
   },
   mounted() {
     this.renderChart(this.chartDataConfig, this.options)
